a50bdbab2932c7bcc6fdaf1bc57b04f9057e82d8
Syeda Zubera Jahan <zuberajahan18@gmail.com>
2022-11-25 10:57:57 +1100

    Updated Task-5 with Date/Time

--- script.js
+++ script.js
@@ -58,6 +58,42 @@ dayBox.innerHTML = currentDay;
 monthBox.innerHTML = currentMonth + 1;
 yearBox.innerHTML = currentYear;
 
+// Updated Task-5
+// Added Current Date/Time
+
+setInterval(displayTime, 1000);
+
+function displayTime() {
+  const time = new Date();
+  const day = time.getDate();
+  const month = time.getMonth() + 1;
+  const year = time.getFullYear();
+  const currentDate = day + '/' + month + '/' + year;
+
+  let hrs = time.getHours();
+  let min = time.getMinutes();
+  let sec = time.getSeconds();
+  let en = 'AM';
+  if (hrs > 12) {
+    en = 'PM';
+    hrs = hrs - 12;
+}
+    if (hrs == 0) {
+  hrs = 12;
+}
+        if (hrs < 10) {
+  hrs = '0' + hrs;
+}
+    if (min < 10) {
+  min = '0' + min;
+}
+    if (sec < 10) {
+  sec = '0' + sec;
+}
+document.getElementById('cDateTime').innerHTML = `${currentDate} ${hrs}:${min}:${sec} ${en}`;
+};
+
+
 //Task 6
 // Create a TaskManager class
 class TaskManger {
